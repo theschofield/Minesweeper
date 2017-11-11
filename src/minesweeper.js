d0d8804d762bda5f5df915a511999e6dd0688e27
theschofield <edjscho@gmail.com>
2017-11-10 17:23:30 -0800

    randomly generating a bomb board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,14 +1,41 @@
-const blankLine = '  |   |  ';
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    };
+    board.push(row);
+  };
+  return board;
+};
 
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    };
+    board.push(row);
+  };
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+      let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+      let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+      // The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
+  };
+  return board;
+};
 
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
+const printBoard = (board) => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
 
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ' + printBoard(playerBoard));
+console.log('Bomb Board: ' + printBoard(bombBoard));
